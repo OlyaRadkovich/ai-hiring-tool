66feb3d404b35cb6f2aa9130760a6edb432ac51d
gygamyt <gygamyt@gmail.com>
2025-10-02 19:48:21 +0200

    feat(redis): i wanna die; main

--- backend/worker_main.py
+++ backend/worker_main.py
@@ -1,5 +1,4 @@
 import os
-
 from fastapi import FastAPI, status
 from loguru import logger
 from redis import from_url
@@ -7,34 +6,34 @@ from rq import Worker
 
 app = FastAPI(
     title="AI Hiring Tool - On-Demand Worker",
-    description="Этот сервис принимает HTTP-запросы для запуска обработки задач из очереди Redis.",
+    description="HTTP-Redis worker",
     version="1.0.0"
 )
 
-redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
-conn = from_url(redis_url)
-listen_queues = ["results_processing"]
+REDIS_URL = os.getenv("REDISURL", "redis://localhost:6379")
+conn = from_url(REDIS_URL)
+
+LISTEN_QUEUES = ["results", "processing"]
 
 
 @app.post("/process", status_code=status.HTTP_202_ACCEPTED)
-def trigger_processing():
-    """
-    Этот эндпоинт принимает 'пинок' от основного бэкенда
-    и запускает обработку очереди в режиме 'burst'.
-    """
-    logger.info("Получен триггер, запускаю обработку очереди...")
+async def trigger_processing():
+    logger.info("Received processing trigger")
     if not conn:
-        logger.error("Нет подключения к Redis. Обработка невозможна.")
+        logger.error("Redis connection is not available")
         return {"status": "error", "detail": "Redis connection not available"}
 
     try:
-        worker = Worker(queues=listen_queues, connection=conn)
-
+        worker = Worker(queues=LISTEN_QUEUES, connection=conn)
         did_process_anything = worker.work(burst=True)
-
-        logger.info(f"Обработка очереди завершена. Были ли обработаны задачи: {did_process_anything}")
+        logger.info(f"Worker processed queue: {did_process_anything}")
         return {"status": "ok", "processed": did_process_anything}
-
     except Exception as e:
-        logger.error(f"Ошибка при обработке очереди: {e}", exc_info=True)
+        logger.error(f"Error processing queue: {e}", exc_info=True)
         return {"status": "error", "detail": str(e)}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
